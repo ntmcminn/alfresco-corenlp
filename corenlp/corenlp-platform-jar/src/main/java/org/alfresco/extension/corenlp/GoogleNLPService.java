de634f38f0346f073e36a33528aefc8b8306eaec
Nathan McMinn <nathan.mcminn@alfresco.com>
2017-05-12 08:57:58 -0500

    Stub for Google NLP test

---
+++ corenlp/corenlp-platform-jar/src/main/java/org/alfresco/extension/corenlp/GoogleNLPService.java
@@ -0,0 +1,38 @@
+package org.alfresco.extension.corenlp;
+
+import org.alfresco.service.ServiceRegistry;
+import org.alfresco.service.cmr.repository.NodeRef;
+
+public class GoogleNLPService implements NLPService {
+
+	@Override
+	public void annotateDocument(NodeRef doc) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setServiceRegistry(ServiceRegistry registry) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setClientFactory(CoreNLPClientFactory clientFactory) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setMaxPages(int maxPages) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setMaxSize(int maxSize) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
