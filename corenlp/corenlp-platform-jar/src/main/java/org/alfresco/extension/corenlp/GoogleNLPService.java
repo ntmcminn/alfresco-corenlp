cc3602461312f09f61641ac86355659dc51bc8c0
Nathan McMinn <nathan.mcminn@alfresco.com>
2017-05-17 14:30:38 -0500

    Switched GoogleNLPService to use direct httpclient calls instead of Google SDK

--- corenlp/corenlp-platform-jar/src/main/java/org/alfresco/extension/corenlp/GoogleNLPService.java
+++ corenlp/corenlp-platform-jar/src/main/java/org/alfresco/extension/corenlp/GoogleNLPService.java
@@ -1,23 +1,36 @@
 package org.alfresco.extension.corenlp;
 
-import java.io.IOException;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
 
-import com.google.cloud.language.spi.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.AnalyzeEntitiesResponse;
-import com.google.cloud.language.v1.Document;
-import com.google.cloud.language.v1.Document.Type;
-import com.google.cloud.language.v1.EncodingType;
 import com.google.cloud.language.v1.Sentiment;
 
 
 public class GoogleNLPService extends AbstractNLPService {
 
-	public void annotateDocument(NodeRef doc) {
+	private String url = "https://language.googleapis.com/v1/documents:analyzeEntities";
+	private String bearer = "";
 
+	
+	public void annotateDocument(NodeRef doc) {
+	
 		try {
 			// Instantiates a client
+		    /*
+		     * Can't use this yet, due to Alfresco shipping with an obsolete Guava version, should
+		     * be updated in the product soon.
+		     * 
 		    LanguageServiceClient language = LanguageServiceClient.create();
 		    String text = getText(doc);
 		    Document procdoc = Document.newBuilder()
@@ -28,9 +41,38 @@ public class GoogleNLPService extends AbstractNLPService {
 		    AnalyzeEntitiesResponse response = language.analyzeEntities(procdoc, encodingType);
 		    
 		    Sentiment sentiment = language.analyzeSentiment(procdoc).getDocumentSentiment();
-		}
-		catch(IOException ioex) {
+		    
+		    */
+			String text = getText(doc);
 			
+			HttpClient client = HttpClients.createDefault();
+			HttpPost post = new HttpPost(url);
+			post.addHeader("Authorization", "Bearer " + bearer);
+			post.addHeader("Content-Type", "application/json");
+			
+			JsonObject entityreq = Json.createObjectBuilder()
+					.add("document", Json.createObjectBuilder()
+						.add("type", "PLAIN_TEXT")
+						.add("content", StringEscapeUtils.escapeJson(text)))
+					.add("encodingType", "UTF8")
+					.build();
+			
+			StringEntity entity = new StringEntity(entityreq.toString());
+			post.setEntity(entity);
+			
+			HttpResponse res = client.execute(post);
+			
+			if(res.getStatusLine().getStatusCode() == 200) {
+				HttpEntity e = res.getEntity();
+				
+				JsonReader resreader = Json.createReader(e.getContent());
+				JsonObject nlpdata = resreader.readObject();
+				
+			}
+			
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
 		}
 
 	}
